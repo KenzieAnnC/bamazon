e66bb108053957bbdab0b553517b11bf68004df5
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-06-05 11:01:13 -0500

    fixes readProd func

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,7 @@
+/////////////////////////////////////////////////
+//        requiring things we need             //
+/////////////////////////////////////////////////
+
 var inquirer = require('inquirer');
 var mysql = require("mysql");
 
@@ -15,44 +19,52 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+/////////////////////////////////////////////////
+//   connecting to sql to list products        //
+/////////////////////////////////////////////////
+
 connection.connect(function (err) {
     if (err) throw err;
-    purchaseItem();
+    readProducts();
 });
+
 
 function readProducts() {
     console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-
-        console.log(res);
+    connection.query("SELECT * FROM products", function (error, response) {
+        for (var i = 0; i < response.length; i++) {
+            console.log(response[i].id);
+            console.log("Name: " + response[i].product_name);
+            console.log("Price: " + response[i].price);
+            console.log('------------------------------------------------------');
+        }
+        if (error) throw error;
+        // console.log(JSON.stringify(response, null, 2));
+        // console.log(res.id + '\n' + "Name: " + res.product_name + '\n' + "Price: " + res.price);
         connection.end();
     });
+    // purchaseItem();
+
 }
 
 function purchaseItem() {
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        readProducts();
-        inquirer
-            .prompt([
-
-                {
-                    name: 'productName',
-                    type: 'input',
-                    message: "Enter the ID of the product you'd like to buy"
-                },
-                {
-                    name: 'productQuantity',
-                    type: 'input',
-                    message: 'How many would you like to purchse?',
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
-                        }
-                        return false;
+    inquirer
+        .prompt([
+            {
+                name: 'productName',
+                type: 'input',
+                message: "Enter the ID of the product you'd like to buy"
+            },
+            {
+                name: 'productQuantity',
+                type: 'input',
+                message: 'How many would you like to purchse?',
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
                     }
+                    return false;
                 }
-            ]);
-    });
+            }
+        ]);
 }
