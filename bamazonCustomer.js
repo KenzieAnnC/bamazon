e4d7d4d7b47b25c2cb4b453429f249ed915119cb
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-06-05 15:19:49 -0500

    fixes param typo >:(

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,8 +23,8 @@ var connection = mysql.createConnection({
 //   connecting to sql to list products        //
 /////////////////////////////////////////////////
 
-connection.connect(function (err) {
-    if (err) throw err;
+connection.connect(function (error) {
+    if (error) throw error;
     start();
 });
 
@@ -55,7 +55,7 @@ function start() {
         {
             name: 'productQuantity',
             type: 'input',
-            message: 'How many would you like to purchse?',
+            message: 'How many would you like to purchase?',
             validate: function (value) {
                 if (isNaN(value) === false) {
                     return true;
@@ -67,25 +67,46 @@ function start() {
         console.log('Product = ' + answer.productID + '\nQuantity = ' + answer.productQuantity);
 
         var product = answer.productID;
-        var quantity = answer.productQuantity;
+        var orderQuantity = answer.productQuantity;
         var query = "SELECT * FROM bamazon_db.products";
 
         connection.query(query, { id: product }, function (error, response) {
             if (error) throw error;
+
             if (response.length === 0) {
                 console.log('No such product! Enter a valid ID');
                 readProducts();
 
             } else {
 
-                if (quantity <= response[0].quantity) {
+                if (orderQuantity <= response[0].quantity) {
                     console.log('Product ordered!');
-                } 
+
+                }
                 else {
                     console.log("\n We dont have enough! Try aomething else");
-                
                 }
+                updateQuantity(product, orderQuantity);
             }
+        });
+    });
+}
+
+function updateQuantity(productID, units) {
+    var query = "SELECT * FROM bamazon_db.products WHERE ?";
+    connection.query(query, { id: productID }, function (error, response) {
+        if (error) throw error;
+
+        console.log(productID);
+        var newQuantity = response[0].quantity - units;
+
+        if (newQuantity < 0)
+            newQuantity = 0;
+
+
+        var newQuery = "UPDATE bamazon_db.products SET ? WHERE ?"
+        connection.query(newQuery, [{ quantity: newQuantity }, { id: productID }], function (error, response) {
+            readProducts();
         });
 
     });
