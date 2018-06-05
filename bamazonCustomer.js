2461a4be2e6665dcc2f1a61b40dc4472814f09aa
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-06-05 13:15:57 -0500

    adding code even though it doesnt work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,7 +25,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    readProducts();
+    start();
 });
 
 
@@ -36,35 +36,66 @@ function readProducts() {
             console.log(response[i].id);
             console.log("Name: " + response[i].product_name);
             console.log("Price: " + response[i].price);
+            console.log("Quantity: " + response[i].quantity);
             console.log('------------------------------------------------------');
         }
         if (error) throw error;
-        // console.log(JSON.stringify(response, null, 2));
-        // console.log(res.id + '\n' + "Name: " + res.product_name + '\n' + "Price: " + res.price);
-        connection.end();
     });
-    // purchaseItem();
-
 }
 
-function purchaseItem() {
-    inquirer
-        .prompt([
-            {
-                name: 'productName',
-                type: 'input',
-                message: "Enter the ID of the product you'd like to buy"
-            },
-            {
-                name: 'productQuantity',
-                type: 'input',
-                message: 'How many would you like to purchse?',
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
+function start() {
+    readProducts();
+
+    inquirer.prompt([
+        {
+            name: 'productID',
+            type: 'input',
+            message: "Enter the ID of the product you'd like to buy"
+        },
+        {
+            name: 'productQuantity',
+            type: 'input',
+            message: 'How many would you like to purchse?',
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (answer) {
+        console.log('Product = ' + answer.productID + '\nQuantity = ' + answer.productQuantity);
+
+        var product = answer.productID;
+        var quantity = answer.productQuantity;
+        var query = "SELECT * FROM bamazon_db.products";
+
+        connection.query(query, { id: product }, function (error, response) {
+            if (error) throw error;
+            if (response.length === 0) {
+                console.log('No such product! Enter a valid ID');
+                readProducts();
+
+            } else {
+
+                if (quantity <= response.quantity) {
+                    console.log('Product ordered!');
+
+                    var newQuery = 'UPDATE bamazon_db.products SET quantity = ' + (response.quantity - answer.productQuantity) + ' WHERE id = ' + product;
+
+                    // Update the inventory
+                    connection.query(newQuery, function (error, response) {
+                        if (error) throw error;
+
+                        console.log(product);
+                    })
+                } 
+                else {
+                    console.log("\n We dont have enough! Try aomething else");
+                
                 }
             }
-        ]);
+        });
+
+    });
 }
